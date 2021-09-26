a79a87bfe4bd534870116f5fe896809162f6d038
Anmol Manchanda <manchandaanmol@icloud.com>
2021-09-26 19:25:59 -0400

    Initial Commit

---
+++ Lab2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab2
+//
+//  Created by user197898 on 9/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
